d59df6d630021e45be075154681e9b1c5abe5d77
Xeviousbr <xeviousbr@gmail.com>
2022-06-20 01:15:18 -0300

    Adicionar arquivos de projeto.

---
+++ Captureitor/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Captureitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 243);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Captureitor/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Captureitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(int keyCode);
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            const short Vlr = -32768;
+            bool KShift = (GetAsyncKeyState(160) == Vlr); // VK_LSHIFT 0xA0
+            bool KControl = (GetAsyncKeyState(162) == Vlr); // VK_LCONTROL  0xA2
+            bool KA = (GetAsyncKeyState(65) == Vlr); // A
+            bool KQ = (GetAsyncKeyState(81) == Vlr); // Q
+            if (KControl & KShift)
+            {
+                if (KA)
+                {
+                    Console.WriteLine("Ligar");
+                } else
+                {
+                    if (KQ)
+                    {
+                        Console.WriteLine("Desligar");
+                    }
+                }
+            }
+        }
+    }
+}
